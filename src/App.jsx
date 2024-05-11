d7f6f36a222a15e2ec982b73a4b54cebba8c5223
fentahunM <fantahun360@gmial.com>
2024-05-11 23:30:05 +0300

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
