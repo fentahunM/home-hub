5692d04c8492da118e8ca7c5e9fee7b94162aa20
fentahunM <fantahun360@gmial.com>
2024-05-27 22:58:44 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// password tL3R66HxBbF8g6HL
+mongoose
+  .connect(process.env.MONGO_LOCAL, {
+    dbName: "users",
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
