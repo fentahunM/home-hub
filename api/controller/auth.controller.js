7fa66c9b30a6a515343dd7bcb35133dad0c6df51
fentahunM <fantahun360@gmial.com>
2024-05-29 10:37:37 +0300

    create middleware and function to handle errors

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,8 +1,9 @@
 import express from "express";
 import User from "../models/user.model.js";
 import bycrypt from "bcryptjs";
+import errorHandler from "../utils/error.js";
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
   const hashedPassword = bycrypt.hashSync(password, 10);
   const newUser = new User({ name, email, password: hashedPassword });
@@ -11,7 +12,8 @@ const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // next(errorHandler(550, error.message));
+    next(error);
   }
 };
 
