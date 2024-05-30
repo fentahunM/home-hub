9078770752bf7a3227012c16cb104400eabb19af
fentahunM <fantahun360@gmial.com>
2024-05-30 16:17:22 +0300

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -2,8 +2,9 @@ import express from "express";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import errorHandler from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-const signup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -17,4 +18,18 @@ const signup = async (req, res, next) => {
   }
 };
 
-export default signup;
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const validUser = await User.findOne({ email });
+  if (!validUser) {
+    return next(errorHandler(404, "User not found"));
+  }
+  const validPassword = bcrypt.compareSync(password, validUser.password);
+  if (!validPassword) {
+    return next(errorHandler(401, "Invalid Credentials"));
+  }
+  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+  const { password: pass, ...rest } = validUser._doc;
+  res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+};
