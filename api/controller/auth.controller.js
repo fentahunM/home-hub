8defe1f5cfed8283fe7176017df855f0f7547d7f
fentahunM <fantahun360@gmial.com>
2024-05-30 16:49:55 +0300

    sign in completed

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -20,16 +20,22 @@ export const signup = async (req, res, next) => {
 
 export const signin = async (req, res, next) => {
   const { email, password } = req.body;
-
-  const validUser = await User.findOne({ email });
-  if (!validUser) {
-    return next(errorHandler(404, "User not found"));
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (err) {
+    next(err);
   }
-  const validPassword = bcrypt.compareSync(password, validUser.password);
-  if (!validPassword) {
-    return next(errorHandler(401, "Invalid Credentials"));
-  }
-  const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-  const { password: pass, ...rest } = validUser._doc;
-  res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
 };
