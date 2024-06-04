6b558068592166a276e42da5288c623613dd6cd7
fentahunM <fantahun360@gmial.com>
2024-06-04 10:06:01 +0300

    complete profile page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 function Profile() {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user.user);
+  return (
+    <div className="p-3 mx-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold my-7 text-center">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full w-24 h-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border rounded-lg p-3  "
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border rounded-lg p-3  "
+        />
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          className="border rounded-lg p-3  "
+        />
+        <button className="bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className=" text-red-700  cursor-pointer">Delete an account</span>
+        <span className=" text-red-700  cursor-pointer ">Sign out</span>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
