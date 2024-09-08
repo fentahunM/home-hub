c7d8d7fabbbd510997ce2035f301b4e68e9c99a2
fentahunM <fantahun360@gmail.com>
2024-09-08 20:47:51 +0300

    add custom font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      openSans: ["Open Sans", "sans-serif"],
+    },
   },
   plugins: [
     require("@tailwindcss/line-clamp"),
