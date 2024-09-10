e359d3e03b531519065d524662df4d6ab4980ea8
fentahunM <fantahun360@gmail.com>
2024-09-10 23:36:23 +0300

    remove about route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
 import Profile from "./pages/Profile.jsx";
 import SignIn from "./pages/SignIn.jsx";
 import SignUp from "./pages/SignUp.jsx";
@@ -17,7 +16,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         <Route path="/sign-in" element={<SignIn />} />
